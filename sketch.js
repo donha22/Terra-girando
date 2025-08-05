3e351b5d5f3cc2a87a00f2a81c35760012485111
donha <miguel.moreira.silva22@escola.pr.gov.br>
2025-08-05 09:28:20 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,13 +1,30 @@
+let terra;
+let estrelas = [];
+let numEstrelas = 500;
+
 function setup() {
-  createCanvas(1920, 1080, WEBGL); //plano 3D
-  terra=loadImage('terra.jpg')
+  createCanvas(1920, 1080, WEBGL);
+  terra = loadImage('terra.jpg');
+  
+  for (let i = 0; i < numEstrelas; i++) {
+    estrelas[i] = {
+      x: random(-width / 2, width / 2),
+      y: random(-height / 2, height / 2),
+      z: random(-1000, 1000)
+    };
+  }
 }
 
 function draw() {
-  background(0, 0, 0); //cor de fundo
-  rotateY(millis()/800); // rotação da terra
-  texture(terra); //textura da terra, imagem.
-  sphere(80, 100); //esfera 3D
+  background(0, 0, 0);
   
+  for (let i = 0; i < numEstrelas; i++) {
+    stroke(255);
+    strokeWeight(2);
+    point(estrelas[i].x, estrelas[i].y, estrelas[i].z);
+  }
   
+  rotateY(millis() / 8000);
+  texture(terra);
+  sphere(80, 100);
 }
